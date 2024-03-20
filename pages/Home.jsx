ad1b5f68584f4c498242718caf0b847b4e009166
victor <109164737+VictorrOG19@users.noreply.github.com>
2024-03-20 19:01:27 +0000

    New changes

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,19 +2,26 @@ import * as React from "react";
 import "../components/App.css";
 
 function Home() {
+  const [isCheckboxActived, SetCheckBox] = React.useState(false);
+  const verifyCheckbox = () => {
+    SetCheckBox(!isCheckboxActived)
+  }
+
   return (
     <div class="container">
       <div class="item-container">
         <p>Usuário</p>
-        <form>
-          <input type="text" class="input"></input>
-        </form>
-        <p>Senha</p>
-        <form>
-          <input type="password" class="input"></input>
-        </form>
-        <form>
-        <input type="checkbox" name="Guardar minha Sessão."></input>
+          <form>
+            <input type="text" class="input"></input>
+          </form>
+            <p>Senha</p>
+          <form>
+            <input type="password" class="input"></input>
+          </form>
+          <form>
+            <label>
+              <input type="checkbox" onChange={verifyCheckbox} checked={isCheckboxActived}/>
+            Guardar minha Sessão.</label>
         </form>
       </div>
    <button id="input-button">Confirmar</button>
